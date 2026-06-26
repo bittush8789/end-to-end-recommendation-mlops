bdc4275ecd2ac7ef421335c6a74c752c67a51b50
Bittu Sharma <bittush9534@gmail.com>
2026-06-27 02:02:40 +0530

    chore: add MLOps CI/CD infrastructure on cicd branch

---
+++ terraform/variables.tf
@@ -0,0 +1,66 @@
+variable "aws_region" {
+  description = "AWS region for all resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project identifier prefix for resource naming"
+  type        = string
+  default     = "ecommerce-mlops"
+}
+
+variable "environment" {
+  description = "Deployment environment (dev / staging / prod)"
+  type        = string
+  default     = "prod"
+}
+
+variable "eks_cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.29"
+}
+
+variable "eks_node_instance_type" {
+  description = "EC2 instance type for EKS worker nodes"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "eks_desired_nodes" {
+  type    = number
+  default = 2
+}
+
+variable "eks_min_nodes" {
+  type    = number
+  default = 1
+}
+
+variable "eks_max_nodes" {
+  type    = number
+  default = 5
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.10.0/24", "10.0.11.0/24"]
+}
+
+variable "mlflow_db_password" {
+  description = "Password for MLflow PostgreSQL backend"
+  type        = string
+  sensitive   = true
+  default     = "MLflowSecurePass123!"
+}
